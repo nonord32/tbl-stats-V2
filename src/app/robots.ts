66ed14bf0ebecec1255e1a455989ae3530760174
nonord32 <nonord32@gmail.com>
2026-04-13 21:45:56 -0400

    Deleted

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-// src/app/robots.ts
-import { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://tblstats.com/sitemap.xml',
-  };
-}
